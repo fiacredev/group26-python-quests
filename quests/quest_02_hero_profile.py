50123089a448f942333bfeb547bb811963711632
Fiacre Dev <123460024+fiacredev@users.noreply.github.com>
2026-06-30 15:19:17 +0200

    Add hero profile script for Question 2

---
+++ quests/quest_02_hero_profile.py
@@ -0,0 +1,7 @@
+# Question 2 in level 1
+
+hero_name = "Nova"
+superpower = "controlling lightning"
+
+print("Meet", hero_name + "!")
+print(hero_name, "has the amazing superpower of", superpower + ".")

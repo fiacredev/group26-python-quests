68882ba21445f1b132a2eadda6d54073ac3f0a50
Fiacre Dev <123460024+fiacredev@users.noreply.github.com>
2026-06-30 15:22:35 +0200

    Remove questions and print statements from quest script
    Removed all questions and related print statements from the quest script.

--- quests/quest_01_first_spell.py
+++ quests/quest_01_first_spell.py
@@ -2,35 +2,3 @@
 
 print("Welcome, brave adventurer!")
 print("Your quest is about to begin. May your code be bug-free and your discoveries legendary!")
-
-# Question 2 in level 1
-
-hero_name = "Nova"
-superpower = "controlling lightning"
-
-print("Meet", hero_name + "!")
-print(hero_name, "has the amazing superpower of", superpower + ".")
-
-# Question 3 in level 1
-
-age = 18
-favorite_food = "pizza"
-
-print("I am", age, "years old and my favorite food is", favorite_food + ".")
-
-# Question 4 in level 1
-
-city_name = "Kigali"
-current_year = 2026
-your_name = "Alex"
-
-print(f"Welcome to {city_name}! The year is {current_year}, and our newest resident is {your_name}.")
-
-# Question 5 in level 1
-
-player_health = 100
-
-player_health -= 25
-player_health += 10
-
-print("Final player health:", player_health)
